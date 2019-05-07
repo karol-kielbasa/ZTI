979678b6d7db2ff7afce426c558b710e284f1a4a
KAROL\Kiełbasa Karol <karsen1995@gmail.com>
2019-05-07 21:26:38 +0200

    added basic validation for user model

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check')
 
 
-// @route   GET api/user
-// @desc    Test
+// @route   POST api/user
+// @desc    Register user
 // @route   Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is not valid').isEmail(),
+    check('password', 'Password must be with 6 or more characters').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array });
+    }
+    res.send('User route')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //mongo
 connectDB();
 
+//middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 //routes
